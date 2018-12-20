e079effa4f0ee1ca62a49a1645b3d3a2283e6c98
ChangHow <ChangHow>
2018-12-20 14:27:16 +1100

    changing workspace

---
+++ Environment.py
@@ -0,0 +1,11 @@
+class Environment:
+    """ Environment is a window with width x height number of blocks """
+
+    def __init__(self, width, height):
+        # initialise the width and height of 2D environment
+        self.width = width
+        self.height = height
+
+    def setup(self):
+        # create an array of position with width x height
+        pass
---
+++ simulation.py
@@ -0,0 +1,15 @@
+"""
+Simulation of Bees choosing from 2 directions based on scent/color
+
+What is required:
+Environment
+    X,Y Plane
+BeeHives
+    Bees
+        X, Y Plane
+Flowers
+    Scent
+    Color
+    X, Y Plane
+"""
+import numpy as np
